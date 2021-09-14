911834d5721d33962ca6d571dc44adea63d9d721
Suraj Kakkad <surajkakkad@Surajs-MacBook-Pro.local>
2021-09-14 19:26:47 +0530

    initial commit

---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ee3926c1-f222-47e1-88df-7963dd547b55'
+};
---
+++ frontend/src/lib/user.ts
@@ -0,0 +1,8 @@
+    // src/lib/user.ts
+    
+    import { writable } from 'svelte/store';
+    import type { User } from './types';
+    
+    const user = writable<User | null>(null);
+    
+    export default user;
